e4c6de7af5e344d33e9b2a5451c5240c3cddbe3f
Nivaldo Acevedo <73075662+N1VTron1@users.noreply.github.com>
2023-04-24 19:22:24 +0000

    made changes

--- 02_02b/index.php
+++ 02_02b/index.php
@@ -1,9 +1,23 @@
 <?php
 
-function print_array($a) {
+function print_array($a)
+{
 	echo '<pre>';
-	var_dump( $a );
+	var_dump($a);
 	echo '</pre>';
 }
 
 $dir = 'files';
+
+$files = scandir($dir);
+$extensions = array();
+
+foreach ($files as $file) {
+	$ext = pathinfo($file, PATHINFO_EXTENSION);
+	if (!empty($ext)) {
+		#shorthand if statement; if extensions[ext] = null, assign 0 to it; otherwise, use current value and add 1
+		$extensions[$ext] = ($extensions[$ext] ?? 0) + 1;
+	}
+}
+
+print_array($extensions);
--- 02_03b/index.php
+++ 02_03b/index.php
@@ -5,5 +5,20 @@ function print_array($a) {
 	echo '</pre>';
 }
 
+#compares two elements of array
+function compare_files($a, $b) {
+	$a = pathinfo($a);
+	$b = pathinfo($b);
+
+	if (strcmp( $a['extension'], $b['extension']) == 0) {
+		return strcmp( $a['filename'], $b['filename']);
+	}
+	return strcmp($a['extension'], $b['extension']);
+}
+
 $dir = 'files';
 $files = scandir($dir);
+
+usort($files, 'compare_files'); #user sort; user defines how array is sorted
+# second parameter 'compare_files' tells PHP to look for compare_files function
+print_array($files);
